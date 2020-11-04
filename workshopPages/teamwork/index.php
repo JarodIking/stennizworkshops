c5da437cb776d2268dfeef447c690170a45350d3
jarod <jarodiking17@gmail.com>
2020-11-04 11:41:11 +0100

    version 4.0.2
    added about sections for the workshops

--- workshopPages/teamwork/index.php
+++ workshopPages/teamwork/index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="core/navbar.css">
-        <link rel="stylesheet" href="core/workshop.css">
+        <link rel="stylesheet" href="../workshop.css">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src='core/workshop.js'></script>
 
@@ -83,6 +83,7 @@
 
                     <div id='vidContainer'>
                         <video autoplay muted loop src="media/f.mp4"></video>
+                        <div id='about'></div>
                     </div>
 
                     <div class='textInsert'><div class='textTitle'>Leerdoelen:</div><div id='goals' class='textHolder'></div></div>
