3ec6c60499f94186eaa8eb077d74168b765898ac
jarod <jarodiking17@gmail.com>
2020-11-05 11:08:33 +0100

    version 4.1.1
    updated broken links at contact page by commenting all unnecessary code

--- scriptes/JavaScript/contact.js
+++ scriptes/JavaScript/contact.js
@@ -1,4 +1,7 @@
+
+
 window.addEventListener('load', ()=>{
+
 
 const home = document.getElementById("home");
 const overOns = document.getElementById("overOns");
@@ -6,67 +9,6 @@ const contact = document.getElementById('contact');
 const link1 = document.getElementById('link1');
 const logo = document.getElementById('navbarImg');
 const gallery = document.getElementById('galleryContact');
-
-const carouselSlide = document.querySelector(".slide");
-const carouselImages = document.querySelectorAll('.slide img')
-const prevBtn = document.querySelector("#prev");
-const nextBtn = document.querySelector("#next");
-
-
-//counter
-let counter = 1;
-const size = carouselImages[0].clientWidth * 2;
-
-carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
-
-//button listeners
-nextBtn.addEventListener("click", ()=>{
-    if(counter >= carouselImages.length -2) return;
-
-    console.log(carouselImages[counter].id);
-
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
-});
-
-prevBtn.addEventListener("click", ()=>{
-    if(counter <= 0) return;
-
-    console.log(carouselImages[counter].id);
-
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
-})
-
-carouselSlide.addEventListener('transitionend', ()=>{
-    if(carouselImages[counter].id === 'lastClone' || carouselImages[counter].id === 'secondLastClone'){
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length -2;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
-
-    }
-    if(carouselImages[counter].id === 'firstClone' || carouselImages[counter].id === 'secondFirstClone'){
-        carouselSlide.style.transition = "none";
-        counter = carouselImages.length -counter;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
-
-    }
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -89,13 +31,68 @@ contact.onclick = ()=>{
 }
 
 
-link1.onclick = ()=>{
-    gallery.setAttribute('id', 'galleryWorkshop');
-    document.getElementById('galleryWorkshop').innerHTML='';
-    sessionStorage.setItem("workshop" , 12);
-    $.getScript("../JavaScript/workshop.js")
+// const carouselSlide = document.querySelector(".slide");
+// const carouselImages = document.querySelectorAll('.slide img')
+// const prevBtn = document.querySelector("#prev");
+// const nextBtn = document.querySelector("#next");s
 
-}
+
+// //counter
+// let counter = 1;
+// const size = carouselImages[0].clientWidth;
+
+// carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+// //button listeners
+// nextBtn.addEventListener("click", ()=>{
+//     if(counter >= carouselImages.length -2) return;
+
+//     console.log(carouselImages[counter].id);
+
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     counter++;
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+// });
+
+// prevBtn.addEventListener("click", ()=>{
+//     if(counter <= 0) return;
+
+//     console.log(carouselImages[counter].id);
+
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     counter--;
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+// })
+
+// carouselSlide.addEventListener('transitionend', ()=>{
+//     if(carouselImages[counter].id === 'lastClone' || carouselImages[counter].id === 'secondLastClone'){
+//         carouselSlide.style.transition = "none";
+//         counter = carouselImages.length -2;
+//         carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+//     }
+//     if(carouselImages[counter].id === 'firstClone' || carouselImages[counter].id === 'secondFirstClone'){
+//         carouselSlide.style.transition = "none";
+//         counter = carouselImages.length -counter;
+//         carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+//     }
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
